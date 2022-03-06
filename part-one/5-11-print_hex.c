315bb999bb7577fe094f87b73eef289d44715fa6
sonen <sonen8524@gmail.com>
2022-03-07 02:08:48 +0300

    Modify code to display in both uppercase and lowercase format

--- part-one/5-11-print_hex.c
+++ part-one/5-11-print_hex.c
@@ -9,7 +9,8 @@ int main() {
     num_3 = 1500;
 
     printf("The numbers in x format: \n");
-    printf("The numbers 15, 150, 1500 in hex format are: %x, %x, %x", num_1, num_2, num_3);
+    printf("The numbers 15, 150, 1500 in hex format (uppercase) are: %X, %X, %X \n", num_1, num_2, num_3);
+    printf("The numbers 15, 150, 1500 in hex format (lowercase) are: %x, %x, %x \n", num_1, num_2, num_3);
 
     return 0;
 }
