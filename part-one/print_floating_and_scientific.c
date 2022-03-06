e384e951350d16f3bdb129acb39ee4e648e458f6
sonen <sonen8524@gmail.com>
2022-03-06 21:46:56 +0300

    Add the print_floating_and_scientific

---
+++ part-one/print_floating_and_scientific.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    float dbl_num = 123.456;
+
+    printf("The floating point number is: %f. \n", dbl_num);
+    printf("The scientific format is %e. \n", dbl_num);
+
+    return 0;
+}
