6b8da877fa6c4e69bc297d7abb645b9b78253ac2
sonen <sonen8524@gmail.com>
2022-03-07 02:25:43 +0300

    Add corrections to exercise listing 5-10

--- part-one/5-10-display_and_align.c
+++ part-one/5-10-display_and_align.c
@@ -5,8 +5,8 @@ int main() {
     int int_num1 = 123;
     float flt_num1 = 123.456;
 
-    printf("%-d\n", int_num1);
-    printf("%-4.3f\n", flt_num1);
+    printf("%-3d\n", int_num1);
+    printf("%-6.3f\n", flt_num1);
 
     return 0;
 }
