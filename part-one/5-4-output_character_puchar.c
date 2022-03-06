bc308db4c73c6841519b9ae56c57d917ce31f9a6
sonen <sonen8524@gmail.com>
2022-03-06 23:58:55 +0300

    Add listing 5-4 from hour 5 to output characters

---
+++ part-one/5-4-output_character_puchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    putchar(65);
+        putchar(10);
+            putchar(66);
+                putchar(10);
+            putchar(67);
+        putchar(10);
+    putchar(0);
+
+    return 0;
+}
