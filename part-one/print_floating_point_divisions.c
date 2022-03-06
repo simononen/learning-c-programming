b0a6fab9298dba02efa6f718b694285983fb3e69
sonen <sonen8524@gmail.com>
2022-03-06 20:41:28 +0300

    Add print_floating_point_division

---
+++ part-one/print_floating_point_divisions.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+
+    int int_num1, int_num2, int_num3;
+
+    float flt_num1, flt_num2, flt_num3;
+
+    int_num1 = 32 / 10;
+    flt_num1 = 32 / 10;
+
+    int_num2 = 32.0 / 10;
+    flt_num2 = 32.0 / 10;
+
+    int_num3 = 32 / 10.0;
+    flt_num3 = 32 / 10.0;
+
+    printf("The integer divis of 32/10 is: %d\n", int_num1);
+    printf("The floating-point divis of 32/10 is: %d\n", flt_num1);
+
+    printf("The integer divis of 32.0/10 is: %d\n", int_num2);
+    printf("The floating-point divis of 32.0/10 is: %d\n", flt_num2);
+    
+    printf("The integer divis of 32/10.0 is: %d\n", int_num3);
+    printf("The floating-point divis of 32/10.0 is: %d\n", flt_num3);
+
+    return 0;
+}
