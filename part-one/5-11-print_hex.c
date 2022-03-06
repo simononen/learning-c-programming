7c2a62cf5f44c5ae5854fafb387904cf4c680d1b
sonen <sonen8524@gmail.com>
2022-03-07 01:58:39 +0300

    Add listing 5-11 as exercise to print hex

---
+++ part-one/5-11-print_hex.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    int num_1, num_2, num_3;
+
+    num_1 = 15;
+    num_2 = 150;
+    num_3 = 1500;
+
+    printf("The numbers in x format: \n");
+    printf("The numbers 15, 150, 1500 in hex format are: %x, %x, %x", num_1, num_2, num_3);
+
+    return 0;
+}
