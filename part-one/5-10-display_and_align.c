4305bafa02cf084a1523746d2b78be5404ba560c
sonen <sonen8524@gmail.com>
2022-03-07 01:53:33 +0300

    Add listing 5-10 as exercise for display and align

---
+++ part-one/5-10-display_and_align.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+    int int_num1 = 123;
+    float flt_num1 = 123.456;
+
+    printf("%-d\n", int_num1);
+    printf("%-4.3f\n", flt_num1);
+
+    return 0;
+}
