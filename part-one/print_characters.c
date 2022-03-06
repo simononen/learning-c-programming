1fc7810d4c12399924b41ffc1c25f6c204978d45
sonen <sonen8524@gmail.com>
2022-03-06 21:40:11 +0300

    Add print_characters

---
+++ part-one/print_characters.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+    char seventy_two = 72;
+    char one_hundred_four = 104;
+
+    printf("The character of 72 is: %c. \n", seventy_two);
+    printf("The character of 104 is: %c. \n", one_hundred_four);
+
+    return 0;
+}
