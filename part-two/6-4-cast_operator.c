22fd752b7575116d352c322c584f2ae1be638266
sonen <sonen8524@gmail.com>
2022-03-07 03:50:33 +0300

    Add listing 6-4 for the cast operator

---
+++ part-two/6-4-cast_operator.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    int x, y;
+
+    x = 7;
+    y = 5;
+    printf("Given x = %d, y = %d\n", x, y);
+    printf("x / y produces: %d\n", x / y);
+    printf("(float)x / (float)y produces: %f\n", (float)x / (float)y);
+
+    return 0;
+}
