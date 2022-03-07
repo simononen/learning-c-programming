fc6c8aec9377635e7918b5aac7fa60646f0d4239
sonen <sonen8524@gmail.com>
2022-03-07 05:08:08 +0300

    Add listing 7-2 for while loops from hour 7

---
+++ part-two/7-2-do_while_loop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    int i;
+
+    i = 65;
+
+    do {
+        printf("The numeric value of %c is %d. \n", i, i);
+        i++;
+    } while(i < 72);
+
+    return 0;
+}
